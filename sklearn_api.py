e07bca8a6afccb0b695add1e16241cd36392d64c
nagomiso <daiseigi.giants+github@gmail.com>
2018-12-19 18:04:18 +0900

    Fix to be able convert unknown data.
    - Fix transform method signature (removed `y`).
    - Implement sampling and noize addition to improve generalization performance.

--- sklearn_api.py
+++ sklearn_api.py
@@ -1,65 +1,96 @@
 # coding: utf-8
 from functools import partial
-from itertools import product
+
+from joblib import delayed
+from joblib import Parallel
 
 import numpy as np
 
 from sklearn.base import BaseEstimator
 from sklearn.base import TransformerMixin
 
-from sklearn.model_selection import StratifiedKFold
-
 from sklearn.utils import check_X_y
+from sklearn.utils import chech_array
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_random_state
 
 
 class KNeighborsFeatures(BaseEstimator, TransformerMixin):
     def __init__(
         self,
         n_neighbors=1,
-        n_splits=5,
-        shuffle=False,
-        random_state=None
+        sampling_ratio=0.8,
+        mean=0,
+        sigma=0.1,
+        n_jobs=1,
+        random_state=None,
+        verbose=0
     ):
         self.n_neighbors = n_neighbors
-        self.n_splits = n_splits
-        self.shuffle = shuffle
-        self.random_state = random_state
+        self.sampling_ratio = sampling_ratio
+        self.mean = mean
+        self.sigma = sigma
+        self.n_jobs = n_jobs
+        self.random_state = check_random_state(random_state)
+        self.verbose = verbose
+
+    def _extract_feature_values(self, x):
+        calc_dist = partial(
+            KNeighborsFeatures.__distance, x)
+        x_knn = []
+        feature_append = x_knn.append
+        for X_train in self._X_each_class.values():
+            distances = np.sort(
+                np.array([calc_dist(x_train) for x_train in X_train]))
+            for k in range(self.n_neighbors):
+                feature_append(np.sum(distances[:k + 1]))
+        return np.array(x_knn)
 
     @staticmethod
     def __distance(v, u):
         return np.linalg.norm(v - u)
 
-    def _extract_feature_value(self, vector, X, y, class_label, k):
-        X_target_class = X[y == class_label]
-        euclidean_vec = partial(self.__distance, vector)
-        distances = np.array([
-            euclidean_vec(vector) for vector in X_target_class])
-        return np.sum(np.sort(distances)[:k + 1])
+    def __choice(self, X, sampling_ratio):
+        if sampling_ratio == 1:
+            return X
+        sample_size = int(X.shape[0] * sampling_ratio)
+        idx = self.random_state.choice(
+            X.shape[0], sample_size, replace=False)
+        return X[np.sort(idx), :]
 
-    def _extract_feature_vectors(self, matrix, X, y, class_label, k):
-        _extract_feature_value = partial(
-            self._extract_feature_value,
-            X=X, y=y, class_label=class_label, k=k)
-        return np.array([
-            _extract_feature_value(vector) for vector in matrix])
+    def __set_sampling_ratio_each_class(self, class_set):
+        if isinstance(self.sampling_ratio, float):
+            self._sampling_ratio_each_class = {
+                class_label: self.sampling_ratio
+                for class_label in class_set
+            }
+        elif isinstance(self.sampling_ratio, dict):
+            if set(self.sampling_ratio.keys()).issubset(class_set):
+                self._sampling_ratio_each_class = self.sampling_ratio
 
-    def transform(self, X, y):
+    def __add_gaussian_noize(self, X):
+        noize = self.random_state.normal(
+            self.mean, self.sigma, np.prod(X.shape))
+        return X + noize.reshape(X.shape)
+
+    def fit(self, X, y):
         X, y = check_X_y(X, y, force_all_finite=True)
+        self._dim = X.shape[1]
+        class_set = set(y)
+        self.__set_sampling_ratio_each_class(class_set)
+        self._X_each_class = {
+            class_label: self.__choice(
+                self.__add_gaussian_noize(X[y == class_label]),
+                self._sampling_ratio_each_class[class_label]
+            )
+            for class_label in class_set
+        }
+        return self
 
-        class_labels = set(y)
-        row_size = np.array(X).shape[0]
-        X_knn = np.empty((row_size, len(class_labels) * self.n_neighbors))
-        skf = StratifiedKFold(
-            self.n_splits, self.shuffle, self.random_state)
-
-        for train_idx, test_idx in skf.split(X, y):
-            X_train, y_train = X[train_idx], y[train_idx]
-            X_test = X[test_idx]
-            features = [
-                self._extract_feature_vectors(
-                    X_test, X_train, y_train, class_label, k)
-                for class_label, k in product(
-                    class_labels, range(self.n_neighbors))
-            ]
-            X_knn[test_idx] = np.array(features).T
+    def transform(self, X):
+        check_is_fitted(self, '_X_each_class')
+        X_knn = np.array(
+            Parallel(n_jobs=self.n_jobs, verbose=self.verbose)(
+                [delayed(self._extract_feature_values)(x) for x in X])
+            )
         return X_knn
